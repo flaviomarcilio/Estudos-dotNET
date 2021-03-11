183b5f1db252abdff653af3eb2deaaa92c053652
flaviomarcilio <flaviomarciliodeoliveira@gmail.com>
2021-03-11 12:20:37 -0300

    Arquivos iniciais

---
+++ Bank/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta minhaConta = new Conta(TipoConta.PessoaFisica, 0, 0, "Flávio Marcilio");
+
+            Console.WriteLine(minhaConta.ToString());
+        }
+
+    }
+}
